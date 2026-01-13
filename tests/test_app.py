678926be2bb321b081b4c99d2e5b006db7ee29bd
Akshay Dagadu Sonawane <akshayson2@gmail.com>
2026-01-13 21:06:00 +0100

    removed errors

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,10 @@
-# In tests/test_app.py
-from ..app import add
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from app import add
 
 def test_add():
-    assert add(2, 3) == 5
+    assert add(1, 2) == 3
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
